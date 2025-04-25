ef7eb6681184cca1bbd39cd68158edfab0ab3c3f
Eirik <eirikalv@icloud.com>
2025-04-25 10:53:38 +0200

    fix

--- electromart/src/App.jsx
+++ electromart/src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import EmptyCart from "./pages/EmptyCart";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import OrderConfirmation from "./pages/OrderConfirmation";
-import Profile from "./pages/Profile";
+import Home from "./pages/home";
+import Cart from "./pages/cart";
+import EmptyCart from "./pages/emptycart";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import OrderConfirmation from "./pages/orderconfirmation";
+import Profile from "./pages/profile";
 
 
 export default function App() {
